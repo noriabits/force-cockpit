66694fa105a1558fda2bb30d0e149a475f8cb7e5
Pablo Fernández <pablo@noriabits.com>
2026-04-08 19:05:53 +0200

    DOMParser

--- src/features/utils/yaml-scripts/YamlScriptsService.ts
+++ src/features/utils/yaml-scripts/YamlScriptsService.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
 import xmlFormat from 'xml-formatter';
+import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
 import { createContext, Script } from 'vm';
 import type { ConnectionManager } from '../../../salesforce/connection';
 import { assertApexSuccess, filterUserDebugLines } from '../../apexUtils';
@@ -232,6 +233,8 @@ export class YamlScriptsService {
         path,
         yaml,
         xmlFormat,
+        DOMParser,
+        XMLSerializer,
         xmlEscape: this.xmlEscape,
         setTimeout,
         clearTimeout,
